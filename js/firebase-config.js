f72280cd4b3db544bc6f678f8bd6c7ff43983bc5
junaedsiam <42120162+junaedsiam@users.noreply.github.com>
2020-05-15 21:16:51 +0600

    Security Fix

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyA2ejW0Uv1cA5l3fHHxl0xAChbPh3Q6AdQ",
-    authDomain: "waterquality-9daf8.firebaseapp.com",
-    databaseURL: "https://waterquality-9daf8.firebaseio.com",
-    projectId: "waterquality-9daf8",
-    storageBucket: "waterquality-9daf8.appspot.com",
-    messagingSenderId: "368707572548",
-    appId: "1:368707572548:web:1b06423e74c5f9039bbd11",
-    measurementId: "G-2RF3FR06JQ"
+    apiKey: "SET_YOUR_KEY_HERE",
+    authDomain: "SET_YOUR_KEY_HERE",
+    databaseURL: "SET_YOUR_KEY_HERE",
+    projectId: "SET_YOUR_KEY_HERE",
+    storageBucket: "SET_YOUR_KEY_HERE",
+    messagingSenderId: "SET_YOUR_KEY_HERE",
+    appId: "SET_YOUR_KEY_HERE",
+    measurementId: "SET_KEY_HERE"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
